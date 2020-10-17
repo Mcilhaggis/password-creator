0d01a0de97325d8852e6691c93807a09e617892e
mcilhaggis <rachael.mcilhagga@live.co.uk>
2020-10-17 17:54:48 -0400

    creating new array with user chosen inputs

--- script.js
+++ script.js
@@ -14,28 +14,21 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-// Various Arrays 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-
 //Generate password
 function generatePassword(){
-  var length = promptForNumber();
+  var pwLength = promptForNumber();
   var confirmLower;
   var confirmUpper;
-  var confirmChar;
+  var confirmSpecial
   var confirmNumber;
-
+  
 
 
 //finding out how many characters the user wants in the password
 function promptForNumber(){
-  var length = prompt("Enter a number between 8-128");
-  if (length >= 8 && length <= 128){
-    alert("Perfect! Your password will have " + length + " characters.");
+  var pwLength = prompt("Enter a number between 8-128");
+  if (pwLength >= 8 && length <= 128){
+    alert("Perfect! Your password will have " + pwLength + " characters.");
     return;
   } else if (isNaN(length)){
     alert("That's not a number. Please enter a number from 8-128.");
@@ -43,33 +36,63 @@ function promptForNumber(){
   } else {
     alert("Your number is not in range.");
     promptForNumber();
-
   };
-  }
+}
 
-//Defining parameters of password
+
+//Defining variables
   var confirmLower = confirm("Do you want to include lower case characters?");
   var confirmUpper = confirm("Do you want to include upper case characters?");
   var confirmNumber = confirm("Do you want to include number characters?");
-  var confirmChar = confirm("Do you want to include special characters?");
+  var confirmSpecial = confirm("Do you want to include special characters?");
   
+//If all answers are false, looping back through to get atleast one true response.
+while (confirmLower === false && confirmUpper === false && confirmNumber === false && confirmSpecial === false){
+  alert("You need to select one type of character");
+  var confirmLower = confirm("Do you want to include lower case characters?");
+  var confirmUpper = confirm("Do you want to include upper case characters?");
+  var confirmNumber = confirm("Do you want to include number characters?");
+  var confirmSpecial = confirm("Do you want to include special characters?");
+  }
+
+
+// Various Arrays
+//Should i be usiing a set instead?? 
+var lowerCaseChar=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCaseChar=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numericChar=["1","2","3","4","5","6","7","8","9","0"];
+var specialChar=["!","@","#","$","%","^","&","*","(",")","{","}","|","[","]",";","'",":","<",">","?","/"];
+var passwordPool = [];
+
+
+
+//loop through chosen options to create a password
+function generateChar(){
+  
+
+  if (confirmLower) {
+      passwordPool.push(...lowerCaseChar);
+  }
+  if (confirmUpper) {
+    passwordPool.push(...upperCaseChar);
+  }
+  if (confirmNumber) {
+    passwordPool.push(...numericChar);
+  }
+  if (confirmSpecial) {
+    passwordPool.push(...specialChar);
+  }
+}
+
+
+
+generateChar();
+console.log(passwordPool);
+
+
+//create an array thats a pool for all selected values from char arrays
+  //create a final for loop that math.random.floors the array . the length of pwLength
+//at the end validate that all of the conditions were met. 
 
 }
 
-//generating values 
-function getRandomUpperCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+65);
- };
-
- function getRandomLowerCase(){
-  return String.fromCharCode(Math.floor(Math.random()*26)+97);
-};
-
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random()*10)+48);
-};
-
-function getRandomSymbol(){
-  var symbol = "!@#$%^&*(){}[]=<>/,.|~?";
-  return symbol[Math.floor(Math.random()*symbol.length)];
-};
